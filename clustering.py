8b062636021c96cd15cabf79166c53b2537d3178
Fernando <82894757+FerLRT@users.noreply.github.com>
2025-01-11 13:48:10 +0100

    Updated clustering.py to select clustering method per parameter

--- clustering.py
+++ clustering.py
@@ -1,13 +1,25 @@
+import argparse
 from utils import read_price_data
 from clustering import (
     build_graph_from_cross_elasticities,
-    partition_graph,
+    kernighan_lin_clustering,
+    louvain_spectral_clustering,
     save_subgraph_data,
     determine_graph_limits,
 )
 
 
 def main():
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Run graph clustering algorithms.")
+    parser.add_argument(
+        "--method",
+        choices=["kernighan_lin", "louvain_spectral"],
+        required=True,
+        help="Clustering method to use (kernighan_lin or louvain_spectral)",
+    )
+    args = parser.parse_args()
+
     # Data files
     prices_file = "data\generic\\20_1_80\_elasticity_prices_1.csv"
     cross_elasticity_file = "data\generic\\20_1_80\_cross_elasticity_prices_1.csv"
@@ -25,8 +37,16 @@ def main():
         product_prices
     )
 
-    # Apply the Kernighan-Lin algorithm to partition the graph
-    subgraphs = partition_graph(graph, max_size)
+    # Apply the selected clustering method
+    if args.method == "kernighan_lin":
+        # Apply the Kernighan-Lin algorithm to partition the graph
+        subgraphs = kernighan_lin_clustering(graph, max_size)
+    elif args.method == "louvain_spectral":
+        # Apply the Louvain-Spectral clustering algorithm to partition the graph
+        max_louvain_clusters = num_products // max_size
+        subgraphs = louvain_spectral_clustering(graph, max_size, max_louvain_clusters)
+    else:
+        raise ValueError(f"Unknown method: {args.method}")
 
     # Save the subgraph data
     for i, subgraph in enumerate(subgraphs):
@@ -35,7 +55,7 @@ def main():
             product_prices,
             cross_product_prices,
             output_dir="data/clusters",
-            prefix="cluster",
+            prefix=f"{args.method}_cluster",
             subgraph_index=i + 1,
         )
 
