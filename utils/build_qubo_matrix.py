d750573ec18582245cdfc229a22308f491f515ac
Fernando <82894757+FerLRT@users.noreply.github.com>
2025-01-12 00:07:59 +0100

    Updated lambda names to be more descriptive

--- utils/build_qubo_matrix.py
+++ utils/build_qubo_matrix.py
@@ -6,10 +6,10 @@ def build_qubo_matrix(
     product_prices,
     cross_product_prices,
     min_margins,
-    lambda_rule1=1,
-    lambda_rule2=5000,
-    lambda_rule3=1,
-    lambda_rule4=1000,
+    lambda_maximize_margins=1,
+    lambda_price_uniqueness=5000,
+    lambda_elasticity=1,
+    lambda_force_price_product=1000,
 ):
     Q = defaultdict(int)
 
@@ -19,7 +19,7 @@ def build_qubo_matrix(
     # Rule 1: Maximize margins
     for (i, p), margin in product_prices.items():
         var = f"i{i}_p{p}"
-        Q[(var, var)] -= lambda_rule1 * (margin - min_margins[i])
+        Q[(var, var)] -= lambda_maximize_margins * (margin - min_margins[i])
 
     # Rule 2: Price uniqueness
     for i in product_ids:
@@ -28,7 +28,7 @@ def build_qubo_matrix(
         for var1 in product_vars:
             for var2 in product_vars:
                 if var1 != var2:
-                    Q[(var1, var2)] += lambda_rule2
+                    Q[(var1, var2)] += lambda_price_uniqueness
 
     # Rule 3: Elasticity is introduced
     for i1, i2, p in cross_product_prices:
@@ -39,7 +39,7 @@ def build_qubo_matrix(
                     var1 = f"i{i1}_p{p}"
                     var2 = f"i{i2}_p{p2}"
                     Q[(var1, var2)] -= (
-                        lambda_rule3
+                        lambda_elasticity
                         * product_prices[(i2, p2)]
                         * margin_percentage
                         / 100
@@ -50,6 +50,6 @@ def build_qubo_matrix(
         product_vars = [f"i{i}_p{p}" for p in price_ids if (i, p) in product_prices]
 
         for var1 in product_vars:
-            Q[(var1, var1)] -= lambda_rule4
+            Q[(var1, var1)] -= lambda_force_price_product
 
     return Q
