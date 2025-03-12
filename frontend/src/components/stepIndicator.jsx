60954ef8ec8abd28cc8edbaab7a7634c539a9529
Fernando <82894757+FerLRT@users.noreply.github.com>
2025-03-12 16:31:16 +0100

    Updated styles for stepIndicator

--- frontend/src/components/stepIndicator.jsx
+++ frontend/src/components/stepIndicator.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaCheck } from "react-icons/fa";
 import "../styles/stepIndicator.css";
 
 export default function StepIndicator({
@@ -8,18 +9,28 @@ export default function StepIndicator({
   completedSteps,
   loadingSteps,
 }) {
+  const stepLabels = ["Introducir datos", "Clustering", "Resultados"];
+
   return (
     <div className="step-indicator-container">
       {steps.map((step, index) => (
-        <div
-          key={index}
-          className={`step-indicator 
+        <div key={index} className="step-wrapper">
+          <div
+            className={`step-indicator 
               ${index === currentStep ? "active" : ""} 
-              ${completedSteps[index] ? "clickable" : ""}
+              ${completedSteps[index] ? "completed" : ""}
               ${loadingSteps[index] ? "loading" : ""}`}
-          onClick={() => completedSteps[index] && setCurrentStep(index)}
-        >
-          {step}
+            onClick={() => completedSteps[index] && setCurrentStep(index)}
+          >
+            {loadingSteps[index] && <div className="loading-border"></div>}
+            {completedSteps[index] ? (
+              <FaCheck className="icon" />
+            ) : (
+              <p className="step-number">{step}</p>
+            )}
+          </div>
+
+          <div className="step-label">{stepLabels[index]}</div>
         </div>
       ))}
     </div>
